aba7c8513eeb6426d3a3d59517cf49452fc57c68
takee <takee@WyattsPC>
2025-10-13 20:13:27 -0700

    Created Footer, AboutWindow Version 1.1

--- project_Charlie/src/main/Main.java
+++ project_Charlie/src/main/Main.java
@@ -1,5 +1,16 @@
 package main;
 
+import controller.*;
+import utils.*;
+import view.*;
+import model.*;
+
+
 public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
